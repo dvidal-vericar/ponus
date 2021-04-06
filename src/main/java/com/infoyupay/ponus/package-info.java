5aa2942a00a387d8b27418c08a206a1723b6846a
David E. Vidal Escudero <david.vidal.escudero@gmail.com>
2021-04-05 23:50:01 -0500

    Added JavaFX dependencies to create first Dashboard form fxml.

---
+++ src/main/java/com/infoyupay/ponus/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The main (root) package for the app.
+ *
+ * @version 1.0
+ * @since 1.0
+ */
+package com.infoyupay.ponus;
---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * The Ponus project main module.
+ *
+ * @since 1.0
+ */
+module ponus {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.web;
+}
